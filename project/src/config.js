29de5b42767ff88ef6ece5dd7ad00cd08987247a
Rifzky Nurman <rifzkynurman11@gmail.com>
2024-07-31 20:52:10 +0700

    perbaiki folder pages

--- project/src/config.js
+++ project/src/config.js
@@ -1,12 +1,5 @@
 const config = {
-<<<<<<< HEAD
-    SERVER_URL: 'http://192.168.137.44:5001'
+    SERVER_URL: 'http://192.168.1.12:5001'
 };
 
 export default config;
-=======
-    SERVER_URL : 'http://10.224.21.22:5001'
-}
-
-export default config
->>>>>>> 0b07facd4208fdca572331c88c1c369f2780102f
