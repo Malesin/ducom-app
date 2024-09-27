304d6d9a3115a1c120a60a61a9a2c15fbc3e6a94
Mika Geshan <mikageshan@gmail.com>
2024-09-27 14:44:10 +0700

    halo

--- project/src/components/LikeNotification.js
+++ project/src/components/LikeNotification.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import DefaultAvatar from '../assets/profilepic.png';
 
@@ -7,17 +7,53 @@ const LikeNotification = ({ notification }) => {
   const handleNotificationPress = () => {
     console.log('Notification pressed');
   };
+  const formatDate = dateString => {
+    const date = new Date(dateString);
+    const now = new Date();
+    const diffInSeconds = Math.floor((now - date) / 1000);
+
+    if (diffInSeconds < 1) {
+      return 'now';
+    }
+
+    if (diffInSeconds < 60) {
+      return `${diffInSeconds}s ago`;
+    }
+
+    const diffInMinutes = Math.floor(diffInSeconds / 60);
+    if (diffInMinutes < 60) {
+      return `${diffInMinutes}m ago`;
+    }
+
+    const diffInHours = Math.floor(diffInMinutes / 60);
+    if (diffInHours < 24) {
+      return `${diffInHours}h ago`;
+    }
+
+    const diffInDays = Math.floor(diffInHours / 24);
+    if (diffInDays < 7) {
+      return `${diffInDays}d ago`;
+    }
+
+    const diffInWeeks = Math.floor(diffInDays / 7);
+    if (diffInWeeks < 4) {
+      return `${diffInWeeks}w ago`;
+    }
+
+    return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+  };
 
   return (
-    <View style={styles.card}>
+    <SafeAreaView style={styles.card}>
       <TouchableOpacity onPress={handleNotificationPress}>
         <View style={styles.notificationRow}>
           <MaterialCommunityIcons name="heart" size={30} color="#E0245E" style={styles.heartIcon} />
           <Image source={notification.like.user.profilePicture ? { uri: notification.like.user.profilePicture } : DefaultAvatar} style={styles.avatar} />
-          <Text style={styles.userName}>{notification.like.user.name} liked your post</Text>
+          <Text style={styles.userName}>@{notification.like.user.username} liked your post.</Text>
+          <Text style={styles.date}> {formatDate(notification.like.created_at)} </Text>
         </View>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -43,15 +79,15 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   avatar: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
+    width: 40,
+    height: 40,
+    borderRadius: 20,
     marginRight: 12,
   },
   userName: {
     fontSize: 16,
     fontWeight: 'bold',
-    color: '#040608',
+    color: '#000',
   },
 });
 
