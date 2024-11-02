4487895dc3f1d913dce7abe3da624a17c7272d24
Mika Geshan <mikageshan@gmail.com>
2024-11-02 11:46:44 +0700

    new features

---
+++ project/src/components/Community/CommunityCard.js
@@ -0,0 +1,160 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+  Share,
+} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const CommunityCard = () => {
+  const [liked, setLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(0);
+  const [bookmarked, setBookmarked] = useState(false);
+  const [bookMarksCount, setBookMarksCount] = useState(0);
+  const [reposted, setReposted] = useState(false);
+  const [repostsCount, setRepostsCount] = useState(0);
+  const [commentsCount] = useState(0);
+
+  const handleLike = () => {
+    if (liked) {
+      setLiked(false);
+      setLikesCount(prevLikesCount => prevLikesCount - 1);
+    } else {
+      setLiked(true);
+      setLikesCount(prevLikesCount => prevLikesCount + 1);
+    }
+  };
+
+  const handleBookmark = () => {
+    if (bookmarked) {
+      setBookmarked(false);
+      setBookMarksCount(prevBookmarksCount => prevBookmarksCount - 1);
+    } else {
+      setBookmarked(true);
+      setBookMarksCount(prevBookmarksCount => prevBookmarksCount + 1);
+    }
+  };
+
+  const handleRepost = () => {
+    if (reposted) {
+      setReposted(false);
+      setRepostsCount(prevRepostsCount => prevRepostsCount - 1);
+    } else {
+      setReposted(true);
+      setRepostsCount(prevRepostsCount => prevRepostsCount + 1);
+    }
+  };
+
+  const handleShare = () => {
+    console.log('share');
+  };
+
+  const handlePress = () => {
+    console.log('konsol');
+  };
+
+  const InteractionButton = ({icon, color, count, onPress}) => (
+    <TouchableOpacity style={styles.actionButton} onPress={onPress}>
+      <MaterialCommunityIcons name={icon} size={20} color={color} />
+      <Text style={styles.actionText}>{count}</Text>
+    </TouchableOpacity>
+  );
+
+  return (
+    <SafeAreaView style={styles.card}>
+      <View style={styles.userInfo}>
+        <MaterialCommunityIcons
+          name="account-multiple"
+          size={15}
+          color="#000"
+        />
+        <TouchableOpacity onPress={handlePress}>
+          <Text style={styles.userName}>Nama Komunitas</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handlePress}>
+          <Text style={styles.userHandle}>@nama_komunitas</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.communityDescription}>
+        Deskripsi komunitas di sini.
+      </Text>
+      <View style={styles.actions}>
+        <InteractionButton
+          icon={liked ? 'heart' : 'heart-outline'}
+          color={liked ? '#E0245E' : '#040608'}
+          count={likesCount}
+          onPress={handleLike}
+        />
+        <InteractionButton
+          icon="message-reply-outline"
+          color="#040608"
+          count={commentsCount}
+          onPress={handlePress}
+        />
+        <InteractionButton
+          icon={bookmarked ? 'bookmark' : 'bookmark-outline'}
+          color={bookmarked ? '#00c5ff' : '#040608'}
+          count={bookMarksCount}
+          onPress={handleBookmark}
+        />
+        <InteractionButton
+          icon="repeat-variant"
+          color={reposted ? '#097969' : '#040608'}
+          count={repostsCount}
+          onPress={handleRepost}
+        />
+        <TouchableOpacity style={styles.actionButton} onPress={handleShare}>
+          <MaterialCommunityIcons
+            name="export-variant"
+            size={20}
+            color="#657786"
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderColor: '#E1E8ED',
+  },
+  userInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  userName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#040608',
+    paddingHorizontal: 5,
+  },
+  userHandle: {
+    color: '#718096',
+    fontSize: 14,
+  },
+  communityDescription: {
+    fontSize: 14,
+    color: '#040608',
+    paddingVertical: 5,
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  actionButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  actionText: {
+    marginLeft: 4,
+    color: '#040608',
+  },
+});
+
+export default CommunityCard;
--- project/src/pages/Community/CommunityScreen.js
+++ project/src/pages/Community/CommunityScreen.js
@@ -1,12 +1,89 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import CommunityCard from '../../components/CommunityCard'
+import React from 'react';
+import {View, Text, FlatList, StyleSheet, SafeAreaView} from 'react-native';
+import CommunityExplore from '../../components/Community/CommunityExplore';
+import CommunityCard from '../../components/Community/CommunityCard';
 
-export default function CommunityScreen() {
+const CommunityScreen = () => {
+  const data = [
+    {
+      id: '1',
+      communityName: 'Komunitas A',
+      memberCount: '1.2K Members',
+      description: 'Deskripsi komunitas A.',
+    },
+    {
+      id: '2',
+      communityName: 'Komunitas B',
+      memberCount: '800 Members',
+      description: 'Deskripsi komunitas B.',
+    },
+    {
+      id: '3',
+      communityName: 'Komunitas C',
+      memberCount: '2.5K Members',
+      description: 'Deskripsi komunitas C.',
+    },
+    {
+      id: '4',
+      communityName: 'Komunitas D',
+      memberCount: '1K Members',
+      description: 'Deskripsi komunitas D.',
+    },
+  ];
+
+  const renderItem = ({item}) => (
+    <CommunityExplore
+      communityName={item.communityName}
+      memberCount={item.memberCount}
+      description={item.description}
+    />
+  );
+
   return (
-    <View>
-      <Text>CommunityScreen</Text>
-      <CommunityCard />
-    </View>
-  )
-}
+    <SafeAreaView style={styles.container}>
+      <View style={styles.exploreContainer}>
+        <Text style={styles.exploreText}>Explore</Text>
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{paddingHorizontal: 0}}
+        />
+      </View>
+      <View style={styles.communityCardContainer}>
+        <CommunityCard />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default CommunityScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  exploreContainer: {
+    paddingVertical: 15,
+    paddingHorizontal: 5,
+    backgroundColor: '#fff',
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  exploreText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#000',
+    marginBottom: 15,
+  },
+  exploreScroll: {
+    paddingVertical: 5,
+  },
+  communityCardContainer: {
+    flex: 1,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+});
